d29cedc471cd88de3e2c0ccca6dea00c64efeb9f
wengjingying <wengjingying@gmail.com>
2022-08-11 10:26:14 -0700

    update for unvbalanced and noised data

--- DataG.R
+++ DataG.R
@@ -147,4 +147,66 @@ DataGeneratorL<-function(beta0,#vector of NO parameter population means, when X=
 
 
 
+#----------------- Unbalanced, noised flow, FeNo data -------------------#
+#Use given flow or randomly mutated unbalanced, noised flow
+
+# assume 10% of the flow were 'missing'
+# generate random vector as the indicator whether the flow were measured
+# Multiply the indicator with the flow matrix
+
+
+FlowFun<-function(Ndat,P){
+  flow=matrix((rep(c(30,30,50,50,100,100,300,300),times=Ndat)+rnorm(Ndat*8,0,1))*rbinom(Ndat*8, 1, 0.9),nrow=Ndat,ncol=8,byrow = T)
+  return(flow)
+}
+
+DataGeneratorCS_UbN<-function(Ndat=10,            # number of study participants
+                          alpha=c(1.5,3.5,2.5),# vector of NO parameter population means, when X=0, in this order: CA, logCaw, logDaw
+                          Xfn=function(n){rnorm(n,0,1)},# function to generate participant-level covariate X as function of number of study participants
+                          beta=c(0.1,0.1,0.1),
+                          flowTarget=c(rep(30,2),rep(50,2),rep(100,2),rep(300,2)),# vector of regression coefficients relating X to each NO parameter, in this order: CA, logCaw, logDaw
+                          Flow=FlowFun(10,0.9), # Must be a Ndat*max length of flow matrix, missing values were 0s
+                          SD=0.1,              #SD of the error
+                          sdalphaCa            = 0.45, # population SD of CA
+                          sdalphalogCaw        = 0.65, # population SD of logCaw
+                          sdalphalogDaw        = 0.55, # population SD of logDaw
+                          coralphalogCawCa     = 0.66, # correlation of NO parameters: logCaw, CA
+                          coralphalogCawlogDaw = -0.35,# correlation of NO parameters: logCaw, logDaw
+                          coralphalogDawCa     = -0.38 # correlation of NO parameters: logDaw, CA
+){
+  # generate X
+  X <- Xfn(Ndat)
+
+  # create variance/covariance matrix for NO parameters based on SD and correlations
+  NOcov <- matrix(c(sdalphaCa^2, coralphalogCawCa*sdalphaCa*sdalphalogCaw, coralphalogDawCa*sdalphaCa*sdalphalogDaw,          
+                    coralphalogCawCa*sdalphaCa*sdalphalogCaw, sdalphalogCaw^2, coralphalogCawlogDaw*sdalphalogCaw*sdalphalogDaw,
+                    coralphalogDawCa*sdalphaCa*sdalphalogDaw, coralphalogCawlogDaw*sdalphalogCaw*sdalphalogDaw, sdalphalogDaw^2),
+                  3,3)
+  logeno <- matrix(NA,ncol=length(flowTarget),nrow=Ndat) # create empty 2-D array for log(FeNO), each row for one participant
+  colnames(logeno) <- flowTarget
+  NOparam <- matrix(NA,ncol=3,nrow=Ndat)           # create empty 2-D array for NO parameters, each row for one participant
+  colnames(NOparam) <- c("Ca","logCaw","logDaw")
+  
+  for(i in 1:Ndat){
+    repeat{
+      param <- mvrnorm(1, X[i] *  beta + alpha, NOcov) # sample NO parameters (Ca, logCaw, logDaw) via multi-normal distribution for participant i
+      FeNOi  <- exp(param[2])+(param[1]-exp(param[2]))*exp(-1*exp(param[3])/Flow[i,]) # calculate mean FeNO for participant i using Ca[i], Caw[i], Daw[i]
+      logFeNOobsi  <- ifelse(FeNOi>0,log(FeNOi) + rnorm(length(Flow[i,]),0,SD),NA) # calculate log(FeNO) for each measurement using mean FeNO if mean FeNO is non-negative
+      if(!any(is.na(logFeNOobsi)) & param[1]>0){break}#satisfy all constrains: Ca[i] is non negative (param[1]) and FeNO[i,j] is non-negative
+    }
+    logeno[i,]  <- logFeNOobsi # store sampled logFeNO for participant i's multi-flow measurement
+    NOparam[i,] <- c(param[1],param[2],param[3]) #store sampled NO parameter for participant i
+  }
+  # create alternative format dataset for frequentist methods
+  logenodata<-melt(data.frame(id=c(1:Ndat),logeno),id="id")
+  logenodata<-logenodata[order(logenodata$id,decreasing = FALSE),]
+  dat <- data.frame(id=rep(c(1:Ndat),each=length(flowTarget)),eno=NA,logeno=NA,flow=as.vector(t(Flow)))
+  dat$logeno  <- logenodata$value
+  dat$eno     <-exp(dat$logeno)
+  dat$flowTarget <- rep(flowTarget,times=Ndat)
+ 
+  dat<-dat[(dat$flow != 0)>0,]
+  # "real" data will have observed flow different from target flow rates
+  return(list(dat=dat,datNOparam=NOparam,datX=data.frame(id=1:Ndat,X=X)))
+}
 
--- TS_HB.R
+++ TS_HB.R
@@ -160,5 +160,164 @@ TSHB_iter<-function(beta0_prior=c(2,4,3),
 }
 
 
+##-----------unbalanced data --------------##
+TSHB_JagsModel_UB<-function()  {# subject:i, flows: list of flow for each subjects
+  for( i in 1 : Ndat) {
+    for( j in offset[i]:(offset[i+1]-1)) {
+      logeno[j] ~ dnorm(logmu[j],tau_c)#logmu new
+      
+      logmu[j]<-log(max(mu[j] ,0.001))#logFeNO>0.001
+      mu[j] <- exp(logCaw[i])+(Ca[i]-exp(logCaw[i]))*exp(-1*exp(logDaw[i])/flow[j])
+    }
+    # store NO parameters
+    Ca[i]       <- param_subject[i, 1]
+    logCaw[i]   <- param_subject[i, 2] 
+    logDaw[i]   <- param_subject[i, 3] 
+    #------------sample bivariate normal (logCaw, logDaw) and zero-truncated normal Ca|(logCaw, logDaw)
+    # add randomness of participant i to NO participants
+    # use truncated normal distribution for Ca
+    param_subject[i,1] ~ dnorm(mu1_22[i],tau1_22);T(0,)
+    # calculated the conditional mean for Ca|(logCaw, logDaw)
+    mu1_22[i]<-beta0_Ca+part1[1,1]*(param_subject[i,2]-beta0_logCaw)+part1[1,2]*(param_subject[i,3]-beta0_logDaw)
+    # sample logCaw and logDaw
+    param_subject[i,2:3] ~ dmnorm(c(beta0_logCaw,beta0_logDaw),tau22[1:2,1:2])
+    #--------------#
+  }
+  #------- percision of Ca|(logCaw, logDaw)--------#
+  tau1_22<-1/cov1_22
+  # variance of Ca Conditioned on (logCaw,logDaw)
+  cov1_22[1,1]<-varCa-(covCalogCaw^2*tau22[1,1]+2*covCalogDaw*tau22[2,1]*covCalogCaw+covCalogDaw^2*tau22[2,2])
+  # Sigm(Ca, logCaw, logDaw) * tau(logCaw, logDaw)
+  part1[1,1]<-covCalogCaw*tau22[1,1]+covCalogDaw*tau22[2,1]
+  part1[1,2]<-covCalogCaw*tau22[1,2]+covCalogDaw*tau22[2,2]
+  # Sigma (logCaw, logDaw)
+  tau22[1:2,1:2]<-inverse(cov22[1:2,1:2])
+  #--------------#
+  # calculate covariances
+  cov22[1,1] <- varlogCaw
+  cov22[1,2] <- covlogCawlogDaw
+  cov22[2,1] <- covlogCawlogDaw
+  cov22[2,2] <- varlogDaw
+  
+  covCalogCaw       <-corlogCawCa*sqrt(varCa*varlogCaw)
+  covCalogDaw       <-corlogDawCa*sqrt(varCa*varlogDaw)
+  covlogCawlogDaw   <-corlogCawlogDaw*sqrt(varlogCaw*varlogDaw)
+  # calculate variances and SDs
+  varCa<-1/tauCa
+  varlogCaw<-1/taulogCaw
+  varlogDaw<-1/taulogDaw
+  sdCa     <- sqrt(varCa)
+  sdlogCaw <- sqrt(varlogCaw)
+  sdlogDaw <- sqrt(varlogDaw)
+  sigma_c<-sqrt(1/tau_c)
+  # sample the third correlation, conditioned on the previous two, to assure positive definite
+  corlogDawCa   ~ dunif(L_corlogDawCa,U_corlogDawCa)
+  L_corlogDawCa <- corlogCawCa*corlogCawlogDaw-sqrt((corlogCawlogDaw^2-1)*(corlogCawCa^2-1))
+  U_corlogDawCa <- corlogCawCa*corlogCawlogDaw+sqrt((corlogCawlogDaw^2-1)*(corlogCawCa^2-1))
+  
+  corlogCawCa        ~ dunif(-1,1)
+  corlogCawlogDaw    ~ dunif(-1,1)
+  # sample precision
+  tauCa ~ dgamma(1.0E-3, 1.0E-3)
+  taulogCaw ~ dgamma(1.0E-3, 1.0E-3)
+  taulogDaw ~ dgamma(1.0E-3, 1.0E-3)
+  tau_c       ~ dgamma(1.0E-3, 1.0E-3)##random error in regression
+  # sample population mean
+  beta0_Ca     ~ dnorm(beta0_prior[1],1e-6)
+  beta0_logCaw ~ dnorm(beta0_prior[2],1e-6)
+  beta0_logDaw ~ dnorm(beta0_prior[3],1e-6)
+} 
+
+#####----------------TSHB ub function ----------------------#
+TSHB_iter_UB<-function(beta0_prior=c(2,4,3),
+                    rhat=1.1,addon.iter=6000,Max_update=40,
+                    n.final=6000,N.iterT=6000,N.thinM=2,N.burnin=4000,N.chain=3,
+                    flow=flow,dat=dat,
+                    tracing=c("beta0_Ca","beta0_logCaw","beta0_logDaw",
+                              "Ca","logCaw","logDaw",
+                              "sdCa","sdlogCaw","sdlogDaw","corlogCawCa","corlogCawlogDaw","corlogDawCa",
+                              "sigma_c")
+){
+  # create offset of flows
+  offset<-c(1,cumsum(table(dat$id))+1)
+  
+  # data needed for JAGS model
+  data_TSHB_UB <- list(Ndat=length(table(dat$id)),logeno=dat$logeno,offset=offset,flow=dat$flow,
+                  beta0_prior=beta0_prior)
+
+  # prepare initial values (number of list = number of parallel chains)
+  inits_TSHB <- 
+    list(
+      list(
+        tau_c=runif(1,0.05,400), 
+        beta0_Ca=runif(1,0,5),
+        beta0_logCaw=runif(1,0,5),
+        beta0_logDaw=runif(1,0,5)
+      ),
+      list(
+        tau_c=runif(1,0.05,400),
+        beta0_Ca=runif(1,0,5),
+        beta0_logCaw=runif(1,0,5),
+        beta0_logDaw=runif(1,0,5)
+      ),
+      list(
+        tau_c=runif(1,0.05,400),
+        beta0_Ca=runif(1,0,5),
+        beta0_logCaw=runif(1,0,5),
+        beta0_logDaw=runif(1,0,5)
+      )
+    )
+  
+  # specify which parameters you want to track
+  # Example: Ca for participant i has rowname Ca[i]
+  param_TSHB <-c("beta0_Ca","beta0_logCaw","beta0_logDaw",
+                 "Ca","logCaw","logDaw",
+                 "sdCa","sdlogCaw","sdlogDaw","corlogCawCa","corlogCawlogDaw","corlogDawCa",
+                 "sigma_c")
+  # simulation start time
+  time<-proc.time()[3]
+  # Jags function
+  fit_TSHB<- jags(data=data_TSHB_UB, parameters.to.save=param_TSHB, inits=inits_TSHB,
+                  model.file=TSHB_JagsModel_UB, n.chains=N.chain, n.iter=N.iterT, 
+                  n.burnin=N.burnin,n.thin=N.thinM) 
+  # model converges when Rhat<1.1
+  # record how much iterations and check points are met
+  N_updateT=0
+  N_final=0
+  print(fit_TSHB$BUGSoutput$summary[rownames(fit_TSHB$BUGSoutput$summary) %in% tracing,"Rhat" ])
+  #---------total iterations: [adaptive phase + short update phase] + [long update phase * N_updateT(+ final phase * (N_final-1))] + [final phase]--------#
+  while(N_updateT<Max_update){# we need a maximum iteration to finish the job
+    # check point 1
+    if(max(fit_TSHB$BUGSoutput$summary[rownames(fit_TSHB$BUGSoutput$summary) %in% tracing,"Rhat" ])<rhat){
+      fit_TSHB<-update(fit_TSHB,n.iter=n.final)# if Rhat<1.1, go to the final phase to obtain posterior distributions
+      # count how many times we entered the final phase
+      N_final=N_final+1
+      print(fit_TSHB$BUGSoutput$summary[rownames(fit_TSHB$BUGSoutput$summary) %in% tracing,"Rhat" ])
+      # check point 2: if met, finish simulation
+      if(max(fit_TSHB$BUGSoutput$summary[rownames(fit_TSHB$BUGSoutput$summary) %in% tracing,"Rhat" ])<rhat){
+        print(fit_TSHB$BUGSoutput$summary[rownames(fit_TSHB$BUGSoutput$summary) %in% tracing,"Rhat" ])
+        break
+      }
+    }# either check point 1 or 2 is not met, repeat update phase
+    fit_TSHB<-update(fit_TSHB,n.iter=addon.iter)
+    # count how many update phase we entered
+    N_updateT<-N_updateT+1
+  }
+  
+  # end time
+  timespend<-proc.time()[3]-time
+  # trimmed mean
+  trimmean<-apply(fit_TSHB$BUGSoutput$sims.matrix,2,function(x) mean(x, 0.2))
+  # 99% quantile
+  quantile1_99<-t(apply(fit_TSHB$BUGSoutput$sims.matrix,2,function(x) quantile(x, c(0.005,0.995))))
+  # output result list
+  result<-list(
+    timespend=timespend,
+    summary=cbind(fit_TSHB$BUGSoutput$summary,"trimmean"=trimmean,quantile1_99),
+    Iter_T=N.iterT+N_updateT*addon.iter+n.final*N_final,# total iterations after the main function
+    Iter_track=c(N.iterT=N.iterT,N_updateT=N_updateT,addon.iter=addon.iter,n.final=n.final,N_final=N_final)# total number of update phases and final phases
+  )
+  return(result)
+}
 
 
